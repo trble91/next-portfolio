0dfd301160fda50363835315740d75291844b527
trble91 <daleyaaron@gmail.com>
2023-05-27 12:23:05 -0400

    corrections to _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,7 @@
 import '../styles/globals.css'
 
+// Has to be config as such. To use on all pages. (https://nextjs.org/docs/pages/building-your-application/optimizing/fonts)
+
 function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />
 }
