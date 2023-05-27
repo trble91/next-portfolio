3aa370319503d45ffcc6440c92f553c7c7e13bee
trble91 <daleyaaron@gmail.com>
2023-05-27 12:13:20 -0400

    rearrange folders and files

---
+++ pages/page.js
@@ -0,0 +1,17 @@
+
+
+export default function Home() {
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+     <div className="min-h-screen">
+      <nav className="py-1 mb-4 flex justify-between">
+        <ul className="flex items-center">
+          <li>About</li>
+          <li>Projects</li>
+          <li></li>
+        </ul>
+      </nav>
+     </div>
+    </main>
+  )
+}
