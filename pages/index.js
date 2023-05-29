d62612d01060a7ccf2f0790aca338aad72ca769f
trble91 <daleyaaron@gmail.com>
2023-05-28 23:05:42 -0400

    fixes text color. Now visible

--- pages/index.js
+++ pages/index.js
@@ -44,17 +44,17 @@ export default function Home() {
             </ul>
           </nav>
           <div className="text-center p-10 py-10">
-            <h2 className="text-5xl py-2 text-gray-900 font-medium dark:text-gray-300 md:text-6xl">
+            <h2 className="text-5xl py-2 text-gray-900 font-medium dark:text-gray-800 md:text-6xl">
              Aaron W. Daley
             </h2>
-            <h3 className="text-2xl py-2 dark:text-white md:text-3xl">
+            <h3 className="text-2xl py-2 dark:text-black md:text-3xl">
               FullStack Developer
             </h3>
-            <p className="text-md py-5 leading-8 text-gray-800 dark:text-gray-200 max-w-xl mx-auto md:text-xl">
+            <p className="text-md py-5 leading-8 text-gray-900 dark:text-gray-900 max-w-xl mx-auto md:text-xl">
               A creative problem-solver, software engineer, and Navy veteran 
               providing services for programming and design needs. 
             </p>
-            <div className="text-5xl flex justify-center gap-16 py-3 text-gray-600 dark:text-gray-400">
+            <div className="text-5xl flex justify-center gap-16 py-3 text-gray-600 dark:text-gray-700">
               <a href="https://www.linkedin.com/in/daleyaaron/"> <AiFillLinkedin /> </a>
               <a href="https://github.com/trble91/"> <AiFillGithub /> </a>
               <a href="https://drive.google.com/drive/folders/1Jmh6zD4k_OOsECbl8WJqlIiCJjsULdcq?usp=share_link"> <AiFillCamera /> </a>
